bb8328db77ee4f421f09cc25eefd1320da38ebe7
Alejandro-Suarez-Rincon <alejandrosuarez2032002@gmail.com>
2024-08-28 12:45:04 -0500

    feat: add routes

--- packages/web/src/modules/components/nav/index.tsx
+++ packages/web/src/modules/components/nav/index.tsx
@@ -4,6 +4,8 @@ import { useSelector, useDispatch } from "react-redux";
 import Link from "next/link";
 import { useEffect } from "react";
 import { fetchTranslations } from "@/utils/fetchTranslations";
+import { usePathname } from "next/navigation";
+import { ROUTES_NAV } from "../types/routes";
 
 interface Translation {
   [key: string]: {
@@ -24,6 +26,8 @@ export function Nav() {
     (state: RootState) => state.translations.translations
   ) as Translation;
 
+  const pathname = usePathname();
+
   useEffect(() => {
     fetchTranslations(dispatch);
   }, [dispatch]);
@@ -32,21 +36,58 @@ export function Nav() {
     dispatch(setLanguage(language === "en" ? "es" : "en"));
   };
 
+  const linkClsses = (path: string) => {
+    return pathname === path
+      ? "py-1 px-2 bg-purple-100 border border-purple-200 rounded-md"
+      : "py-1 px-2 hover:bg-purple-50 hover:border border-purple-100 rounded-md";
+  };
+
   return (
-    <header className="flex flex-grow justify-between mx-40 mt-16">
-      <div></div>
-      <div className="flex gap-5 justify-between">
-        <Link href="/">{translations["home"]?.[language] || "Inicio"}</Link>
-        <Link href="">{translations["aboutme"]?.[language] || ""}</Link>
-        <Link href="">{translations["projects"]?.[language] || ""}</Link>
-        <Link href="">{translations["experience"]?.[language] || ""}</Link>
-        <Link href="">{translations["contact"]?.[language] || ""}</Link>
-      </div>
-      <div>
-        <button onClick={toggleLanguage}>
-          {language === "en" ? "Español" : "English"}
-        </button>
-      </div>
-    </header>
+    <section className="flex bg-principal">
+      <header className="flex flex-grow justify-between mx-40 my-16 ">
+        <div></div>
+        <div className="flex gap-5 justify-between">
+          <Link href={ROUTES_NAV.HOME} className={linkClsses(ROUTES_NAV.HOME)}>
+            {translations["home"]?.[language] || "Inicio"}
+          </Link>
+
+          <Link
+            href={ROUTES_NAV.ABOUT_ME}
+            className={linkClsses(ROUTES_NAV.ABOUT_ME)}
+          >
+            {translations["aboutme"]?.[language] || ""}
+          </Link>
+
+          <Link
+            href={ROUTES_NAV.PROJECTS}
+            className={linkClsses(ROUTES_NAV.PROJECTS)}
+          >
+            {translations["projects"]?.[language] || ""}
+          </Link>
+
+          <Link
+            href={ROUTES_NAV.EXPERIENCE}
+            className={linkClsses(ROUTES_NAV.EXPERIENCE)}
+          >
+            {translations["experience"]?.[language] || ""}
+          </Link>
+
+          <Link
+            href={ROUTES_NAV.CONTACT}
+            className={linkClsses(ROUTES_NAV.CONTACT)}
+          >
+            {translations["contact"]?.[language] || ""}
+          </Link>
+        </div>
+        <div>
+          <button
+            onClick={toggleLanguage}
+            className="py-1 px-2 hover:bg-purple-50"
+          >
+            {language === "en" ? "Es" : "En"}
+          </button>
+        </div>
+      </header>
+    </section>
   );
 }
---
+++ packages/web/src/modules/components/types/routes.ts
@@ -0,0 +1,7 @@
+export enum ROUTES_NAV {
+  HOME = "/",
+  ABOUT_ME = "/about-me",
+  PROJECTS = "/projects",
+  EXPERIENCE = "/experience",
+  CONTACT = "/contact",
+}
